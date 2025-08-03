8774c70e2e825fe3887c3343c54ac8a5763ce258
Sanya's Mac <sanya15arora@gmail.com>
2025-08-03 21:35:53 +0530

    Moved frontend to root and removed backend folder

--- backend/src/middleware/verifyAdmin.js
+++
@@ -1,8 +0,0 @@
-const verifyAdmin = async (req, res, next) => {
-    if (req.role !== 'admin') {
-        return res.status(403).send({ success: false, message: 'Access denied. Only Admins are authorized.' });
-    }
-    next();
-}
-
-module.exports = verifyAdmin;
